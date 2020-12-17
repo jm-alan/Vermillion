7e2a5d51e4f834f1d9744f8995ed7043eb0d20ed
JM Alan <j.maxwell.alan@gmail.com>
2020-12-17 01:51:16 -0500

    Complete phase 4

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post('/', asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+  const user = await User.signup({ email, username, password });
+
+  await setTokenCookie(res, user);
+
+  return res.json({ user });
+}));
 
 module.exports = router;
