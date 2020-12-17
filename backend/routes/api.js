6e716ef58b3303d0be7c4befc42c2a65b3f7a001
JM Alan <j.maxwell.alan@gmail.com>
2020-12-17 01:22:52 -0500

    .

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../utils/auth.js');
-const {User} = require('../db/models');
+const { User } = require('../db/models');
 
 module.exports = router;
