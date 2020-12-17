9ba2bfe4f979fb7e1f0a7dedd21a9a3a3d6130cd
JM Alan <j.maxwell.alan@gmail.com>
2020-12-17 02:03:24 -0500

    complete phase 5

---
+++ backend/utils/validation.js
@@ -0,0 +1,52 @@
+const { check } = require('express-validator');
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = require('express-validator')(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array().map(error => `${error.msg}`);
+
+    const err = Error('Bad request.');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad request.';
+    next(err);
+  }
+  next();
+};
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+module.exports = {
+  validateLogin,
+  validateSignup
+};
