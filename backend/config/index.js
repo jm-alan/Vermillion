6a401e1cef6b7fe435206306686da4a8037cc2d6
JM <j.maxwell.alan@gmail.com>
2020-12-23 23:10:03 -0500

    env references updated

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,10 +2,10 @@ module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   db: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABSE,
-    host: process.env.DB_HOST
+    username: process.env.DB_FROGGY,
+    password: process.env.DB_FRESH,
+    database: process.env.DB_KRISPY,
+    host: process.env.DB_KREME
   },
   jwtConfig: {
     secret: process.env.JWT_SECRET,
