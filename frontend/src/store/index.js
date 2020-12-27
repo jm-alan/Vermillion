b86c2b8d375b5bc3e3098e65005f97fa15effb85
JM <j.maxwell.alan@gmail.com>
2020-12-27 09:03:05 -0500

    Clean up redux index to cooperate with Heroku

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,11 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
 const rootReducer = combineReducers({
   session: sessionReducer
 });
 
-const isNotProduction = process.env.NODE_ENV !== 'production';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = isNotProduction
-  ? composeEnhancers(applyMiddleware(thunk, require('redux-logger').default))
-  : applyMiddleware(thunk);
+const enhancer = applyMiddleware(thunk);
 
 const configureStore = (preloadedState) => createStore(rootReducer, preloadedState, enhancer);
 
