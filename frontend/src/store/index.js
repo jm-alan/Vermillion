d02d3d0af8262a069b91272fca6f08bd5ff5eb7c
JM <j.maxwell.alan@gmail.com>
2020-12-28 06:13:57 -0500

    compose?

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
 const rootReducer = combineReducers({
   session: sessionReducer
 });
 
-const enhancer = applyMiddleware(thunk);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const configureStore = (preloadedState) => createStore(rootReducer, preloadedState, enhancer);
 
