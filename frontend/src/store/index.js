5fd415a4edad1244e4fe546652afd73f552c175d
JM <j.maxwell.alan@gmail.com>
2020-12-28 07:07:04 -0500

    reconstruct env-sensitive store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,9 +5,11 @@ const rootReducer = combineReducers({
   session: sessionReducer
 });
 
+const isNotProduction = process.env.NODE_ENV !== 'production';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const enhancer = isNotProduction
+  ? composeEnhancers(applyMiddleware(thunk, require('redux-logger').default))
+  : applyMiddleware(thunk);
 
 const configureStore = (preloadedState) => createStore(rootReducer, preloadedState, enhancer);
 
