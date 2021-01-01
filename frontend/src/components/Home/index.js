f33e03cd6b9d575e8ebd0b2ee2fb9a2d58f89b21
JM <j.maxwell.alan@gmail.com>
2021-01-01 11:22:33 -0500

    file management, post reducer export/import fixed

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 
-import AnonHomePage from '../AnonHomePage';
-import UserHomePage from '../UserHomePage';
+import AnonHomePage from './Anon';
+import UserHomePage from './User';
 
 export default function Home () {
   const user = useSelector(({ session: { user } }) => user);
